1835002f8a6aca2005076b3c3d4b0a4a1fe65e7d
leonardo.ferreira <leonardo.lmf27@gmail.com>
2025-02-19 11:45:44 -0300

    start commit

---
+++ src/main/java/com/br/addc/AddClients/AddClientsApplication.java
@@ -0,0 +1,13 @@
+package com.br.addc.AddClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddClientsApplication.class, args);
+	}
+
+}
